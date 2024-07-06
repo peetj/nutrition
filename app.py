47b411244747c8853004bb535847e8c58e0965de
Pete Januarius <pete@nexgencodecamp.com.au>
2024-07-06 19:12:10 +1000

    removed exposed api keys

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
+import os
 from flask import Flask, render_template, request
 from fatsecret import Fatsecret
 
 app = Flask(__name__)
 
-api_key = 'f3f939e92c2f4102986917ec969b2fe7'
-api_secret = '54fb2c826d3a4b46b280cc76d3164faf'
+api_key = os.getenv('nutrition_api_key')
+api_secret = os.getenv('nutrition_api_secret')
 fs = Fatsecret(api_key, api_secret)
 
 @app.route('/')
